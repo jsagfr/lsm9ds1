036b7a735ea17abf8ea5f6eeda1e8831d64b39f6
Jérôme Guéry <jerome.guery@gmail.com>
2017-03-19 16:34:29 +0100

    add accel_max_g and accel_max_g_available attributes

--- lsm9ds1.c
+++ lsm9ds1.c
@@ -84,6 +84,47 @@
 /* bits */
 #define LSM9DS1_I_AM         0b01101000
 
+#define LSM9DS1_ODR_G_MASK 7 << 5
+#define LSM9DS1_ODR_G_PD   0
+#define LSM9DS1_ODR_G_14_9 1 << 5
+#define LSM9DS1_ODR_G_59_9 2 << 5
+#define LSM9DS1_ODR_G_119  3 << 5
+#define LSM9DS1_ODR_G_238  4 << 5
+#define LSM9DS1_ODR_G_476  5 << 5
+#define LSM9DS1_ODR_G_952  6 << 5
+
+#define LSM9DS1_ODR_XL_MASK 7 << 5
+#define LSM9DS1_ODR_XL_PD   0
+#define LSM9DS1_ODR_XL_10   1 << 5
+#define LSM9DS1_ODR_XL_50   2 << 5
+#define LSM9DS1_ODR_XL_119  3 << 5
+#define LSM9DS1_ODR_XL_238  4 << 5
+#define LSM9DS1_ODR_XL_476  5 << 5
+#define LSM9DS1_ODR_XL_952  6 << 5
+
+#define LSM9DS1_FS_XL_2G  0
+#define LSM9DS1_FS_XL_16G 1 << 3
+#define LSM9DS1_FS_XL_4G  2 << 3
+#define LSM9DS1_FS_XL_8G  3 << 3
+#define LSM9DS1_FS_XL_MASK 0b00011000
+
+#define LSM9DS1_FS_XL_SCALE_2G  IIO_G_TO_M_S_2( 2000000000ULL / S16_MAX)
+#define LSM9DS1_FS_XL_SCALE_4G  IIO_G_TO_M_S_2( 4000000000ULL / S16_MAX)
+#define LSM9DS1_FS_XL_SCALE_8G  IIO_G_TO_M_S_2( 8000000000ULL / S16_MAX)
+#define LSM9DS1_FS_XL_SCALE_16G IIO_G_TO_M_S_2(16000000000ULL / S16_MAX)
+
+#define LSM9DS1_FS_G_245DPS  0
+#define LSM9DS1_FS_G_500DPS  1 << 3
+#define LSM9DS1_FS_G_2000DPS 3 << 3
+#define LSM9DS1_FS_G_MASK    0b00011000
+
+#define LSM9DS1_FS_G_SCALE_245DPS  IIO_DEGREE_TO_RAD( 245000000 / S16_MAX)
+#define LSM9DS1_FS_G_SCALE_500DPS  IIO_DEGREE_TO_RAD( 500000000 / S16_MAX)
+#define LSM9DS1_FS_G_SCALE_2000DPS IIO_DEGREE_TO_RAD(2000000000 / S16_MAX)
+
+#define LSM9DS1_SW_RESET 1
+
+
 
 /* enum { */
 /*         LSM9DS0, */
@@ -103,10 +144,12 @@ struct lsm9ds1_data {
 		.realbits = 12, \
 		.storagebits = 16, \
 		.endianness = IIO_LE, \
-         },                                      \
-	.info_mask_separate = BIT(IIO_CHAN_INFO_RAW), \
+         }, \
+	.info_mask_separate = BIT(IIO_CHAN_INFO_RAW) | \
+                BIT(IIO_CHAN_INFO_PROCESSED), \
         .info_mask_shared_by_type = BIT(IIO_CHAN_INFO_SCALE) | \
-                        BIT(IIO_CHAN_INFO_OFFSET),             \
+                        BIT(IIO_CHAN_INFO_OFFSET) |            \
+                        BIT(IIO_CHAN_INFO_SAMP_FREQ),          \
 }
 
 #define LSM9DS1_CHANNEL_XL(reg, axis) {	\
@@ -120,7 +163,7 @@ struct lsm9ds1_data {
 		.storagebits = 16, \
 		.endianness = IIO_LE, \
          },                                      \
-	.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),	\
+	.info_mask_separate = BIT(IIO_CHAN_INFO_RAW), \
         .info_mask_shared_by_type = BIT(IIO_CHAN_INFO_SCALE), \
 }
 
@@ -150,7 +193,7 @@ static const struct iio_chan_spec lsm9ds1_channels[] = {
 };
 
 static int lsm9ds1_register_mask_write(struct i2c_client *client, u16 addr,
-                                     u8 mask, u8 data)
+                                       u8 mask, u8 data)
 {
         int ret;
         u8 tmp_data = 0;
@@ -176,31 +219,11 @@ static int lsm9ds1_register_reset_bit(struct i2c_client *client, u16 addr,
         return lsm9ds1_register_mask_write(client, addr, 0, mask);
 }
 
-#define LSM9DS1_SW_RESET 1
-
 static int lsm9ds1_reset(struct i2c_client *client)
 {
         return lsm9ds1_register_set_bit(client, LSM9DS1_REG_CTRL_REG8,
                                         LSM9DS1_SW_RESET);
 }
-
-#define LSM9DS1_ODR_G_MASK 7 << 5
-#define LSM9DS1_ODR_G_PD   0
-#define LSM9DS1_ODR_G_14_9 1 << 5
-#define LSM9DS1_ODR_G_59_9 2 << 5
-#define LSM9DS1_ODR_G_119  3 << 5
-#define LSM9DS1_ODR_G_238  4 << 5
-#define LSM9DS1_ODR_G_476  5 << 5
-#define LSM9DS1_ODR_G_952  6 << 5
-
-#define LSM9DS1_ODR_XL_MASK 7 << 5
-#define LSM9DS1_ODR_XL_PD   0
-#define LSM9DS1_ODR_XL_10   1 << 5
-#define LSM9DS1_ODR_XL_50   2 << 5
-#define LSM9DS1_ODR_XL_119  3 << 5
-#define LSM9DS1_ODR_XL_238  4 << 5
-#define LSM9DS1_ODR_XL_476  5 << 5
-#define LSM9DS1_ODR_XL_952  6 << 5
 
 static int lsm9ds1_enable(struct i2c_client *client, bool enable)
 {
@@ -220,25 +243,99 @@ static int lsm9ds1_enable(struct i2c_client *client, bool enable)
 }
 
 
-#define LSM9DS1_FS_XL_2G  0
-#define LSM9DS1_FS_XL_16G 1 << 3
-#define LSM9DS1_FS_XL_4G  2 << 3
-#define LSM9DS1_FS_XL_8G  3 << 3
-#define LSM9DS1_FS_XL_MASK 0b00011000
+static ssize_t
+lsm9ds1_show_accel_max_g(struct device *dev,
+                                struct device_attribute *attr,
+                                char *buf)
+{
+        struct iio_dev *indio_dev = dev_to_iio_dev(dev);
+        struct lsm9ds1_data *data = iio_priv(indio_dev);
+        /* struct lsm9ds1_state *st = iio_priv(indio_dev); */
+        // TODO: spinlock
+        int len = 0, ret;
 
-#define LSM9DS1_FS_XL_SCALE_2G  IIO_G_TO_M_S_2( 2000000 / S16_MAX)
-#define LSM9DS1_FS_XL_SCALE_4G  IIO_G_TO_M_S_2( 4000000 / S16_MAX)
-#define LSM9DS1_FS_XL_SCALE_8G  IIO_G_TO_M_S_2( 8000000 / S16_MAX)
-#define LSM9DS1_FS_XL_SCALE_16G IIO_G_TO_M_S_2(16000000 / S16_MAX)
+        ret = i2c_smbus_read_word_data(data->client, LSM9DS1_REG_CTRL_REG6_XL);
+        if (ret < 0)
+                return ret;
 
-#define LSM9DS1_FS_G_245DPS  0
-#define LSM9DS1_FS_G_500DPS  1 << 3
-#define LSM9DS1_FS_G_2000DPS 3 << 3
-#define LSM9DS1_FS_G_MASK    0b00011000
+        switch ((u8)ret & LSM9DS1_FS_XL_MASK) {
+        case LSM9DS1_FS_XL_2G:
+                len += sprintf(buf + len, "2\n");
+                break;
+        case LSM9DS1_FS_XL_4G:
+                len += sprintf(buf + len, "4\n");
+                break;
+        case LSM9DS1_FS_XL_8G:
+                len += sprintf(buf + len, "8\n");
+                break;
+        case LSM9DS1_FS_XL_16G:
+                len += sprintf(buf + len, "16\n");
+                break;
+        default:
+                return -ENODEV;
+        }
+        return len;
+}
 
-#define LSM9DS1_FS_G_SCALE_245DPS  IIO_DEGREE_TO_RAD( 245000000 / S16_MAX)
-#define LSM9DS1_FS_G_SCALE_500DPS  IIO_DEGREE_TO_RAD( 500000000 / S16_MAX)
-#define LSM9DS1_FS_G_SCALE_2000DPS IIO_DEGREE_TO_RAD(2000000000 / S16_MAX)
+static ssize_t
+lsm9ds1_store_accel_max_g(struct device *dev,
+                                 struct device_attribute *attr,
+                                 const char *buf,
+                                 size_t len)
+{
+        struct iio_dev *indio_dev = dev_to_iio_dev(dev);
+        struct lsm9ds1_data *data = iio_priv(indio_dev);
+        int ret;
+        u8 val;
+
+        ret = kstrtou8(buf, 10, &val);
+        if (ret < 0)
+                return ret;
+
+        switch (val) {
+        case 2:
+                ret = lsm9ds1_register_mask_write(
+                        data->client, LSM9DS1_REG_CTRL_REG6_XL,
+                        LSM9DS1_FS_XL_MASK, LSM9DS1_FS_XL_2G);
+                break;
+        case 4:
+                ret = lsm9ds1_register_mask_write(
+                        data->client, LSM9DS1_REG_CTRL_REG6_XL,
+                        LSM9DS1_FS_XL_MASK, LSM9DS1_FS_XL_4G);
+                break;
+        case 8:
+                ret = lsm9ds1_register_mask_write(
+                        data->client, LSM9DS1_REG_CTRL_REG6_XL,
+                        LSM9DS1_FS_XL_MASK, LSM9DS1_FS_XL_8G);
+                break;
+        case 16:
+                ret = lsm9ds1_register_mask_write(
+                        data->client, LSM9DS1_REG_CTRL_REG6_XL,
+                        LSM9DS1_FS_XL_MASK, LSM9DS1_FS_XL_16G);
+                break;
+        default:
+                return -EINVAL;
+        }
+        return (ret ? ret : len);
+}
+
+static IIO_CONST_ATTR(in_accel_max_g_available, "2 4 8 16");
+
+static IIO_DEVICE_ATTR(accel_max_g, S_IRUGO | S_IWUSR,
+                       lsm9ds1_show_accel_max_g,
+                       lsm9ds1_store_accel_max_g,
+                       0);
+
+
+static struct attribute *lsm9ds1_attributes[] = {
+        &iio_const_attr_in_accel_max_g_available.dev_attr.attr,
+        &iio_dev_attr_accel_max_g.dev_attr.attr,
+        NULL,
+};
+
+static const struct attribute_group lsm9ds1_attribute_group = {
+        .attrs = lsm9ds1_attributes,
+};
 
 static int lsm9ds1_read_raw(struct iio_dev *indio_dev,
                             struct iio_chan_spec const *chan,
@@ -252,7 +349,7 @@ static int lsm9ds1_read_raw(struct iio_dev *indio_dev,
 		ret = i2c_smbus_read_word_data(data->client, chan->address);
 		if (ret < 0)
 			return ret;
-		*val = ret;
+		*val = (s16) ret;
                 *val2 = 0;
 		return IIO_VAL_INT;
         case IIO_CHAN_INFO_SCALE:
@@ -269,21 +366,18 @@ static int lsm9ds1_read_raw(struct iio_dev *indio_dev,
 
                         *val = 0;
                         switch (ret & LSM9DS1_FS_XL_MASK) {
-                        case LSM9DS1_FS_XL_2G:
-                                *val2 = LSM9DS1_FS_XL_SCALE_2G;
-                                return IIO_VAL_INT_PLUS_MICRO;
                         case LSM9DS1_FS_XL_4G:
                                 *val2 = LSM9DS1_FS_XL_SCALE_4G;
-                                return IIO_VAL_INT_PLUS_MICRO;
+                                return IIO_VAL_INT_PLUS_NANO;
                         case LSM9DS1_FS_XL_8G:
                                 *val2 = LSM9DS1_FS_XL_SCALE_8G;
-                                return IIO_VAL_INT_PLUS_MICRO;
+                                return IIO_VAL_INT_PLUS_NANO;
                         case LSM9DS1_FS_XL_16G:
                                 *val2 = LSM9DS1_FS_XL_SCALE_16G;
-                                return IIO_VAL_INT_PLUS_MICRO;
+                                return IIO_VAL_INT_PLUS_NANO;
                         default:
-                                dev_err(&indio_dev->dev, "unaccessible code accessed\n");
-                                return -ENODEV;
+                                *val2 = LSM9DS1_FS_XL_SCALE_2G;
+                                return IIO_VAL_INT_PLUS_NANO;
                         }
                 case IIO_ANGL_VEL:
                         ret = i2c_smbus_read_byte_data(data->client,
@@ -293,9 +387,6 @@ static int lsm9ds1_read_raw(struct iio_dev *indio_dev,
 
                         *val = 0;
                         switch (ret & LSM9DS1_FS_G_MASK) {
-                        case LSM9DS1_FS_G_245DPS:
-                                *val2 = LSM9DS1_FS_G_SCALE_245DPS;
-                                return IIO_VAL_INT_PLUS_MICRO;
                         case LSM9DS1_FS_G_500DPS:
                                 *val2 = LSM9DS1_FS_G_SCALE_500DPS;
                                 return IIO_VAL_INT_PLUS_MICRO;
@@ -303,8 +394,8 @@ static int lsm9ds1_read_raw(struct iio_dev *indio_dev,
                                 *val2 = LSM9DS1_FS_G_SCALE_2000DPS;
                                 return IIO_VAL_INT_PLUS_MICRO;
                         default:
-                                dev_err(&indio_dev->dev, "unaccessible code accessed\n");
-                                return -ENODEV;
+                                *val2 = LSM9DS1_FS_G_SCALE_245DPS;
+                                return IIO_VAL_INT_PLUS_MICRO;
                         }
                 default:
                         return -EINVAL;
@@ -318,6 +409,28 @@ static int lsm9ds1_read_raw(struct iio_dev *indio_dev,
                 default:
                         return -EINVAL;
                 }
+	case IIO_CHAN_INFO_PROCESSED:
+                ret = i2c_smbus_read_word_data(data->client, chan->address);
+
+                if (ret < 0)
+                        return ret;
+                switch (chan->type) {
+                case IIO_TEMP:
+                        *val = (s16)ret / 16 + 25;
+                        *val2 = abs(1000000 * (s16)ret / 16 % 1000000);
+                        return IIO_VAL_INT_PLUS_MICRO;
+                default:
+                        return -EINVAL;
+                }
+        case IIO_CHAN_INFO_SAMP_FREQ:
+                switch (chan->type) {
+                case IIO_TEMP:
+                        *val = 59;
+                        *val2 = 500000;
+                        return IIO_VAL_INT_PLUS_MICRO;
+                default:
+                        return -EINVAL;
+                }
 	default:
 		return -EINVAL;
 	}
@@ -326,6 +439,7 @@ static int lsm9ds1_read_raw(struct iio_dev *indio_dev,
 
 static const struct iio_info lsm9ds1_info = {
 	.driver_module	= THIS_MODULE,
+        .attrs          = &lsm9ds1_attribute_group,
 	.read_raw	= lsm9ds1_read_raw,
 };
 
